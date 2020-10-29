039427d859ac44df79daa257747bc1ddebb42424
Luis <luismamanezginer@gmail.com>
2020-10-29 21:02:32 +0100

    Primer Script Python

---
+++ P1.py
@@ -0,0 +1,9 @@
+"""nombre = input("Cual es tu nombre?\n") 
+print("Hola "+nombre)
+edad =int(input("Cuál es tu edad?\n"))
+edad = edad -10
+print(edad)"""
+Mentira= False
+Verdad = True
+print(Mentira or Verdad)
+
